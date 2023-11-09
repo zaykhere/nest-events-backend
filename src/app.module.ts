05179775176ca6ed087d5fe8f1783a5bc397aabb
zain <zainjhere@gmail.com>
2023-11-09 21:50:49 +0500

    setup dotenv and custom configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Event } from './events/event.entity';
 import { EventsModule } from './events/events.module';
-
+import { ConfigModule } from '@nestjs/config';
+import ormConfig from './config/orm.config';
+import ormConfigProd from './config/orm.config.prod';
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: '127.0.0.1',
-    port: 3306,
-    username: 'root',
-    password: 'Iambest',
-    database: 'nest_events',
-    entities: [Event],
-    synchronize: true
-  }),
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [ormConfig]
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: process.env.NODE_ENV !== 'production' ? ormConfig : ormConfigProd
+    }),
   EventsModule
 ],
   controllers: [AppController],
---
+++ src/config/orm.config.prod.ts
@@ -0,0 +1,14 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { Event } from "src/events/event.entity";
+import {registerAs} from "@nestjs/config"
+
+export default registerAs('orm.config', (): TypeOrmModuleOptions => ({
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  entities: [Event],
+  synchronize: false
+}))  
